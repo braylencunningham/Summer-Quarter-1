30839ba4b9001fbe3af7a34ab3c5aedac422e394
Braylen Cunningham <braylen.cunningham@hgs.hiddengenius.org>
2025-06-26 13:54:27 -0700

    Quick Math + Exit Ticket

---
+++ Python/Week 2/QuikMath2.md
@@ -0,0 +1,11 @@
+1.Define what a function is in your own words
+A block of code that does one job once it's called.
+
+2. What key word is used to define a function?
+def MyFunction
+
+3. What does a return statement do?
+Returns the data when the function is completed
+
+4. What is the difference between arguments and parameters?
+Parameters are named placeholders while arguments are the real values. 
---
+++ Python/Week 2/QuikMath2.py
@@ -0,0 +1,24 @@
+def Add(addend1, addend2):
+    return addend1 + addend2
+
+sum = Add(5, 35)
+print(sum)
+
+def Subtract(number1, number2):
+    return number1 - number2
+
+difference = Subtract(40, 10)
+print(difference)
+
+def Multiply(num1, num2):
+    return num1 * num2
+
+product = Multiply(7, 10)
+print(product)
+
+def Divide(div1, div2):
+    return div1 / div2
+
+quotient = Divide(60, 10)
+print(quotient)
+
